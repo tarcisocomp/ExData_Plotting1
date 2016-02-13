01ed7465aff619f171f8484343d557388732cd63
Tarciso Braz <tarcisocomp@gmail.com>
2016-02-13 18:07:04 -0300

    Adding Plot 2 and its generation Rscript

---
+++ plot2.R
@@ -0,0 +1,28 @@
+#Notice: Assuming the dataset folder is in workspace.
+power_ds <- read.csv2(file="exdata-data-household_power_consumption/household_power_consumption.txt", header = TRUE)
+selected.data <- power_ds[power_ds$Date == "1/2/2007" | power_ds$Date == "2/2/2007",]
+selected.data$Date <- as.Date(as.character(selected.data$Date), "%d/%m/%Y")
+#selected.data$Weekday <- as.factor(weekdays(selected.data$Date, abbreviate = TRUE))
+selected.data$Global_active_power <- as.numeric(as.character(selected.data$Global_active_power))
+selected.data$Timestamp <- as.numeric(as.POSIXct(paste(selected.data$Date, selected.data$Time), 
+                                      format="%Y-%m-%d %H:%M:%S"))
+
+minX = min(selected.data$Timestamp)
+maxX = max(selected.data$Timestamp)
+xRange = maxX - minX
+
+minY = min(selected.data$Global_active_power)
+maxY = max(selected.data$Global_active_power)
+yRange = maxY - minY
+
+png(filename = "plot2.png", width = 480, height = 480)
+
+plot(selected.data$Timestamp, selected.data$Global_active_power, 
+     type="l", axes = FALSE,
+     ylab="Global Active Power (killowatts)",
+     xlab="")
+axis(1,at=seq(minX,maxX,by=xRange/2),labels=c("Thu","Fri","Sat"))
+axis(2,at=seq(0,6,by=2),labels=seq(0,6,by=2))
+box()
+
+dev.off()
